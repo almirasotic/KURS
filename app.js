1766f1d2aa66e308101972b82d0419743be553f7
Almir Asotic <asoica500@gmail.com>
2022-01-21 19:28:16 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// fruits=["Banane", "Kruske", "Mango"];
+// console.log(fruits)
+
+// a=fruits();
+
+// console.log(fruits);
+// console.log()
+
+// fruits=["Banane", "Kruske", "Mango"];
+// //pop metode
+
+// console.log(fruits)
+// returnValue= fruits.pop()
+// console.log(fruits)
+
+let food1=['mango','popcorn','apple'];
+let food2=['kruska','mandarina','krompir'];
+
+let proveriisti=[];
+for(let i=0; i<food1.length;i++)
+{
+    for (let j=0; j<food2.length;j++)
+    {
+        if(food1[i][j]==food2[i][j])
+        commonValueIndex.push=([i][j]);;
+    }
+}
